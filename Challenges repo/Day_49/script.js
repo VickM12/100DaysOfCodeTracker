0c7f113d70505525e23b8680d8e125d2a3b294c3
Vicktor M <vicktor.moberg@gmail.com>
2020-12-17 11:17:05 -0500

    built a memoizer function for fibonacci sequence

---
+++ Challenges repo/Day_49/script.js
@@ -0,0 +1,36 @@
+let fibonacci = (n) =>{
+  if (n <= 1) {
+    return 1
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2)
+  
+};
+console.log(fibonacci(5))
+
+let fibonacci2 = (n2, memo) => {
+  memo = memo || {}
+  if (memo[n2]) {
+    return memo[n2]
+  }
+  if (n2 <= 1) {
+    return 1
+  }; 
+  return memo[n2] = fibonacci2(n2 - 1, memo) + fibonacci2(n2 - 2, memo)
+}
+console.log(fibonacci2(5))
+
+let memoizer = (fun) => {
+  let cache = {}
+  return function (n) {
+    if (cache[n] != undefined) {
+      return cache[n]
+    } else {
+      let result = fun(n)
+      cache[n] = result
+      return result
+    }
+  }
+}
+
+const fibMemoFunction = memoizer(fibonacci)
+console.log(fibMemoFunction(5))
